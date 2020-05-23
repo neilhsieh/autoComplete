3aa47e6cade2b5ceb4987d78e686f7bc64ec0bed
Neil Hsieh <neilhsieh.94@gmail.com>
2020-05-23 11:06:32 -0400

    feat: search added

--- api/server.js
+++ api/server.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const axios = require("axios");
 
-const app = express();
+async function start() {
+  const app = express();
 
-app.listen(8080);
-console.log("API is listening on http://localhost:8080");
+  const {
+    data: { results: users },
+  } = await axios("https://randomuser.me/api/?results=1000");
+
+  const router = express.Router();
+
+  router.get("/search", (req, res, next) => {
+    const { q } = req.query;
+    if (!q) return next(new Error("No query found"));
+
+    const searched = users.filter((u) => {
+      const name = `${u.name.first} ${u.name.last}`.toLowerCase();
+      return name.includes(q.toLowerCase());
+    });
+
+    res.json(searched);
+  });
+
+  app.use(router);
+
+  app.listen(8080);
+  console.log("API is listening on http://localhost:8080");
+}
+
+start();
